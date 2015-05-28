a308bf8f0e1b79d3062a5d89f2788f997d6e2c2a
NENEN <Rik&@a6QwI11>
2015-05-28 19:46:56 -0400

    more fractExposed tables to look at
    spinning out visuals of the original shading tables

--- IC_Solar/Shading_LUT/Generic_8x8/createRestLUTs.m
+++ IC_Solar/Shading_LUT/Generic_8x8/createRestLUTs.m
@@ -1,29 +1,47 @@
 % Given: a quadrant's worth of %shaded LUTs for ICSF
 % This script creates % exposed LUTs, for the og LUTs, and for the other quadrants. 
 %% 
-filenam = 'ICSolarmodule_12_ShadLUT_Generic_Copy.txt';
+filenam = 'ICSolarmodule_09_ShadLUT_Generic.txt';
+% filenam = 'ICSolarmodule_12_ShadLUT_Generic_Copy.txt';
 shadModNum = filenam(15:16);
-
 delimiterIn = '\t';
 headerlinesIn = 1;
 shadLUTdata = importdata(filenam,delimiterIn, headerlinesIn);
-
+L = 49;
 %complement: make this an LUT of "fraction exposed" 
 % rather than "fraction shaded"
-shadLUTdata.data(:,3) = 1-shadLUTdata.data(:,3);
-% here is the original LUT from the file
-ogLUT = rot90(reshape(shadLUTdata.data(:,3),[49 49]),-1);
+fractExposed = 1-shadLUTdata.data(:,3);
+% put the pitch and yaw vectors through same transforms as the data
+og_pitchVec = shadLUTdata.data(:,1);
+og_yawVec = shadLUTdata.data(:,2);
+ 
+% reshape the original data vector from the file
 %now flip left-right because the original file is (72,72)=(up,left)
-ogLUT = fliplr(ogLUT);
+%and here's the original LUT:
+ogLUT = fliplr(rot90(reshape(fractExposed,[L L]),-1));
+% now flip it left-right
+ogLUTPrime = fliplr(ogLUT);
+%reshape the piutch and yaw vectors to match
+og_pitchtable = reshape(og_pitchVec,[L L]);
+og_yawtable = reshape(og_yawVec,[L L]);
+% and shove the tables back into vectors
+fractExposedPrime = reshape(rot90(ogLUTPrime,1),[L*L 1]);
+
+what = fractExposed-fractExposedPrime
+
+ogLUTPrimePrime = flipud(ogLUTPrime);
+ogLUTPrimePrimePrime = flipud(ogLUTPrimePrime);
 
 
 %% xlswrite to confirm
 % xlswrite('C:\Users\Nick\Documents\CASE_to_SCOREC\IC_Solar\Shading_LUT\Generic_8x8\testxlsLUTout.xlsx',ogLUT)
 pitchVector = [72:-3:-72]';
 yawVector = fliplr(pitchVector');
-ssName = strcat('LUTmodule_',shadModNum,'.xlsx');
-xlswrite(ssName,ogLUT,1,'b2')
-xlswrite(ssName,pitchVector,1,'a2')
-xlswrite(ssName,yawVector,1,'b1')
+ssName = strcat('pctExposedLUT_module_',shadModNum,'.xlsx');
+sstag = {strcat('Mod_',shadModNum)};
+xlswrite(ssName,sstag,1,'a1');
+xlswrite(ssName,ogLUT,1,'b2');
+xlswrite(ssName,pitchVector,1,'a2');
+xlswrite(ssName,yawVector,1,'b1');
 
 %% 
